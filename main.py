1e4d0c447a4bda40f51aa337e1a66ae589577f9b
Mukund-patawardhan <61786847+Mukund-patawardhan@users.noreply.github.com>
2022-08-20 14:12:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+
+reader = pd.read_csv('shared_articles.csv')
+data = list(reader)
+all_articles = data[1:]
+
+liked_articles = []
+not_liked_articles = []
+
+app = Flask(__name__)
+
+@app.route("/get-article")
+def get_article():
+    return jsonify({
+        "data": all_article[0],
+        "status": "success"
+    })
+
+@app.route("/liked-article", methods=["POST"])
+def liked_article():
+    article = all_articles[0]
+    all_article = all_article[1:]
+    liked_articles.append(article)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/unliked-article", methods=["POST"])
+def unliked_article():
+    article = all_articles[0]
+    all_article = all_article[1:]
+    not_liked_articles.append(article)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+if __name__ == "__main__":
+  app.run()
